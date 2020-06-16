3b3bf187aee5f8158e4119c92d41c60a64c66cb1
Alexis Ortega <alexis.oc3@gmail.com>
2020-06-16 12:06:20 -0600

    Update login screen

--- app/src/main/java/com/alexiscrack3/spinny/login/LoginActivity.kt
+++ app/src/main/java/com/alexiscrack3/spinny/login/LoginActivity.kt
@@ -5,8 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.alexiscrack3.spinny.R
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        supportActionBar?.show()
     }
 }

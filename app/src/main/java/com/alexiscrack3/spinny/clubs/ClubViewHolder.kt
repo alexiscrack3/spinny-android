939d5cea4437c062071505898762499c4a27fe6d
Alexis Ortega <alexis.oc3@gmail.com>
2020-07-30 17:38:36 -0600

    Navigate to club details screen

--- app/src/main/java/com/alexiscrack3/spinny/clubs/ClubViewHolder.kt
+++ app/src/main/java/com/alexiscrack3/spinny/clubs/ClubViewHolder.kt
@@ -1,6 +1,10 @@
 package com.alexiscrack3.spinny.clubs
 
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.alexiscrack3.spinny.R
+import com.alexiscrack3.spinny.clubs.details.ClubFragment.Companion.CLUB_ID_KEY
 import com.alexiscrack3.spinny.databinding.ClubItemBinding
 import com.alexiscrack3.spinny.models.Club
 
@@ -9,8 +13,15 @@ class ClubViewHolder(
 ) : RecyclerView.ViewHolder(clubItemBinding.root) {
 
     fun bind(club: Club) {
-        clubItemBinding.club = club
-        // This forces the bindings to run immediately instead of delaying them until the next frame
-        clubItemBinding.executePendingBindings()
+        with(clubItemBinding) {
+            this.club = club
+
+            // This forces the bindings to run immediately instead of delaying them until the next frame
+            this.executePendingBindings()
+        }
+        itemView.setOnClickListener { view ->
+            val args = bundleOf(CLUB_ID_KEY to club.id)
+            view.findNavController().navigate(R.id.action_clubsFragment_to_clubFragment, args)
+        }
     }
 }
--- app/src/test/java/com/alexiscrack3/spinny/clubs/ClubViewHolderTest.kt
+++ app/src/test/java/com/alexiscrack3/spinny/clubs/ClubViewHolderTest.kt
@@ -1,5 +1,7 @@
 package com.alexiscrack3.spinny.clubs
 
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import com.alexiscrack3.spinny.R
 import com.alexiscrack3.spinny.SpinnyTest
 import com.alexiscrack3.spinny.databinding.ClubItemBinding
@@ -7,6 +9,8 @@ import com.alexiscrack3.spinny.models.Club
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class ClubViewHolderTest : SpinnyTest() {
@@ -24,5 +28,31 @@ class ClubViewHolderTest : SpinnyTest() {
 
         verify(clubItemBinding).club = club
         verify(clubItemBinding).executePendingBindings()
+    }
+
+    @Test
+    fun `navigate to club details screen when clicking on the view`() {
+        val club = Club("1", "name")
+        val clubItemBinding = mock<ClubItemBinding> {
+            on { this.root } doReturn view
+        }
+        val navController = TestNavHostController(context).apply {
+            setGraph(R.navigation.main_nav_graph)
+        }
+        Navigation.setViewNavController(view, navController)
+        val testObject = ClubViewHolder(clubItemBinding)
+        testObject.bind(club)
+
+        testObject.itemView.performClick()
+
+        assertThat(
+            navController.currentDestination?.id,
+            equalTo(R.id.clubFragment)
+        )
+
+        assertThat(
+            navController.backStack.last().arguments?.getString("CLUB_ID"),
+            equalTo(club.id)
+        )
     }
 }

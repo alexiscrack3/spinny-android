18b84c7eb7b5321766a937b1f402468eded07613
Alexis Ortega <alexis.oc3@gmail.com>
2020-07-30 23:32:58 -0600

    Test getClubById method in clubs service

--- app/src/test/java/com/alexiscrack3/spinny/clubs/ClubsServiceTests.kt
+++ app/src/test/java/com/alexiscrack3/spinny/clubs/ClubsServiceTests.kt
@@ -58,4 +58,37 @@ class ClubsServiceTests {
         assertThat(data.id, equalTo(id))
         assertThat(data.name, equalTo(name))
     }
+    @Test
+    fun `getClubById should return club response`() {
+        val id = "123"
+        val name = "name"
+        val jsonData = """
+        {
+            "data": {
+                "members": [],
+                "_id": "$id",
+                "name": "$name",
+                "created_at": "2020-07-18T22:20:51.574Z"
+            }
+        }
+            """.trimIndent()
+
+        val dispatcher = object : Dispatcher() {
+            @Throws(InterruptedException::class)
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return MockResponse()
+                    .addHeader("Content-Type", "application/json; charset=utf-8")
+                    .setResponseCode(HttpURLConnection.HTTP_OK)
+                    .setBody(jsonData)
+            }
+        }
+        mockWebServer.dispatcher = dispatcher
+        val baseUrl = mockWebServer.url("/")
+        val testObject = ServicesFactory(baseUrl.toString()).createService(ClubsService::class.java)
+
+        val actual = testObject.getClubById(id).blockingGet()
+        val data = actual.data
+        assertThat(data.id, equalTo(id))
+        assertThat(data.name, equalTo(name))
+    }
 }

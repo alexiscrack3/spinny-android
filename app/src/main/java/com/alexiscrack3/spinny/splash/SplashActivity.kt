b55cac5029f2cfd8f3cfd4a882fe4b4bce4339df
Alexis Ortega <alexis.oc3@gmail.com>
2020-08-16 18:38:05 -0600

    Create splash screen the right way

---
+++ app/src/main/java/com/alexiscrack3/spinny/splash/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.alexiscrack3.spinny.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alexiscrack3.spinny.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = LoginActivity.getIntent(this)
+        startActivity(intent)
+        finish()
+    }
+}

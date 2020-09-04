c020af1d7aa34fc1691d1ade23f2012552f927df
Alexis Ortega <alexis.oc3@gmail.com>
2020-09-03 20:09:30 -0600

    Remove unnecessary property

--- app/src/main/java/com/alexiscrack3/spinny/SpinnyFragment.kt
+++ app/src/main/java/com/alexiscrack3/spinny/SpinnyFragment.kt
@@ -1,11 +1,5 @@
 package com.alexiscrack3.spinny
 
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-abstract class SpinnyFragment : Fragment() {
-
-    val supportActionBar: ActionBar?
-        get() = (requireActivity() as? AppCompatActivity)?.supportActionBar
-}
+abstract class SpinnyFragment : Fragment()

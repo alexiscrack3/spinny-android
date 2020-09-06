886043152db1d28dfd9802155391adb940469c98
Alexis Ortega <alexis.oc3@gmail.com>
2020-09-06 13:41:50 -0600

    Add tests for main activity

--- app/src/test/java/com/alexiscrack3/spinny/MainActivityTest.kt
+++ app/src/test/java/com/alexiscrack3/spinny/MainActivityTest.kt
@@ -3,6 +3,7 @@ package com.alexiscrack3.spinny
 import android.view.MenuItem
 import androidx.test.core.app.ActivityScenario
 import com.alexiscrack3.spinny.settings.SettingsActivity
+import com.alexiscrack3.spinny.splash.SplashActivity
 import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
@@ -12,7 +13,7 @@ import org.robolectric.Shadows
 class MainActivityTest : SpinnyTest() {
 
     @Test
-    fun `navigate to settings screen when clicking on the settings menu item`() {
+    fun `navigate to settings screen when selecting menu item for settings`() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         activityScenario.onActivity { activity ->
             val shadowActivity = Shadows.shadowOf(activity)
@@ -24,6 +25,45 @@ class MainActivityTest : SpinnyTest() {
             val actual = shadowActivity.nextStartedActivity
 
             assertThat(actual.component?.className).isEqualTo(SettingsActivity::class.java.name)
+        }
+        activityScenario.close()
+    }
+
+    @Test
+    fun `onOptionsItemSelected should return true when selecting menu item for settings`() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity { activity ->
+            val menuItem = mock<MenuItem> {
+                on { this.itemId } doReturn R.id.settings_activity
+            }
+            val actual = activity.onOptionsItemSelected(menuItem)
+
+            assertThat(actual).isTrue()
+        }
+        activityScenario.close()
+    }
+
+    @Test
+    fun `onOptionsItemSelected should return false when selecting menu item for nothing`() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity { activity ->
+            val menuItem = mock<MenuItem> {
+                on { this.itemId } doReturn 1
+            }
+            val actual = activity.onOptionsItemSelected(menuItem)
+
+            assertThat(actual).isFalse()
+        }
+        activityScenario.close()
+    }
+
+    @Test
+    fun `onSupportNavigateUp should return false when parent activity is null`() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity { activity ->
+            val actual = activity.onSupportNavigateUp()
+
+            assertThat(actual).isFalse()
         }
         activityScenario.close()
     }

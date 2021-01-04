7a5d97b2fc404ba7240d22fb5fb398fc11853c22
Alexis Ortega <alexis.oc3@gmail.com>
2021-01-03 20:46:59 -0700

    Update club's screen

--- app/src/main/java/com/alexiscrack3/spinny/clubs/details/ClubFragment.kt
+++ app/src/main/java/com/alexiscrack3/spinny/clubs/details/ClubFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.OrientationHelper
 import com.alexiscrack3.spinny.R
 import com.alexiscrack3.spinny.SpinnyFragment
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.item_club.*
+import kotlinx.android.synthetic.main.fragment_club.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -32,9 +35,22 @@ class ClubFragment : SpinnyFragment() {
         clubViewModel.getClubId(clubId)
             .subscribeOn(Schedulers.io())
             .subscribe({
-                club_name_text_view.text = it.data.name
+                val players = it.data.members
+                club_players_list.adapter =
+                    ClubPlayersAdapter(players + players + players + players + players + players + players + players + players + players + players + players + players + players + players)
             }, {
                 Timber.e(it)
             })
+
+        val dividerItemDecoration = DividerItemDecoration(
+            requireContext(), OrientationHelper.VERTICAL
+        )
+        club_players_list.addItemDecoration(dividerItemDecoration)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
     }
 }

b6026a7a28c93bda1078dad803b80d193dc2b98e
Alexis Ortega <alexis.oc3@gmail.com>
2021-01-06 13:30:37 -0700

    Use data binding in club fragment

--- app/src/main/java/com/alexiscrack3/spinny/clubs/details/ClubFragment.kt
+++ app/src/main/java/com/alexiscrack3/spinny/clubs/details/ClubFragment.kt
@@ -5,24 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.OrientationHelper
-import com.alexiscrack3.spinny.R
 import com.alexiscrack3.spinny.SpinnyFragment
 import com.alexiscrack3.spinny.api.Resource
 import com.alexiscrack3.spinny.databinding.ClubFragmentBinding
 import com.alexiscrack3.spinny.models.Club
-import kotlinx.android.synthetic.main.fragment_club.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
 class ClubFragment : SpinnyFragment() {
+    private lateinit var binding: ClubFragmentBinding
     private val clubViewModel by viewModel<ClubViewModel>()
     private val clubPlayersAdapter by inject<ClubPlayersAdapter>()
     private val navController by lazy { this.findNavController() }
@@ -52,9 +50,8 @@ class ClubFragment : SpinnyFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<ClubFragmentBinding>(
+        binding = ClubFragmentBinding.inflate(
             inflater,
-            R.layout.fragment_club,
             container,
             false
         ).apply {
@@ -66,13 +63,13 @@ class ClubFragment : SpinnyFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        club_collapsing_toolbar_layout.setupWithNavController(
-            club_toolbar,
+        binding.clubCollapsingToolbarLayout.setupWithNavController(
+            binding.clubToolbar,
             navController,
             appBarConfiguration
         )
-//        club_toolbar.inflateMenu(R.menu.main)
-//        club_toolbar.setOnMenuItemClickListener { item ->
+//        binding.clubToolbar.inflateMenu(R.menu.main)
+//        binding.clubToolbar.setOnMenuItemClickListener { item ->
 //            val navController = findNavController()
 //            item?.onNavDestinationSelected(navController) ?: false
 //        }
@@ -87,8 +84,8 @@ class ClubFragment : SpinnyFragment() {
         val dividerItemDecoration = DividerItemDecoration(
             requireContext(), OrientationHelper.VERTICAL
         )
-        club_players_list.addItemDecoration(dividerItemDecoration)
-        club_players_list.adapter = clubPlayersAdapter
+        binding.clubPlayersList.addItemDecoration(dividerItemDecoration)
+        binding.clubPlayersList.adapter = clubPlayersAdapter
     }
 
     override fun onResume() {

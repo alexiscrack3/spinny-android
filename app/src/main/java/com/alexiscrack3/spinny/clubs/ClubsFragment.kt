6a4e7fc024354028c4a87b40ab8897f36b15a9cb
Alexis Ortega <alexis.oc3@gmail.com>
2023-03-19 22:54:46 -0400

    Format clubs fragment

--- app/src/main/java/com/alexiscrack3/spinny/clubs/ClubsFragment.kt
+++ app/src/main/java/com/alexiscrack3/spinny/clubs/ClubsFragment.kt
@@ -10,11 +10,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alexiscrack3.spinny.R
-import com.alexiscrack3.spinny.api.*
-import com.alexiscrack3.spinny.login.LoginViewModel
-import com.alexiscrack3.spinny.models.Club
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class ClubsFragment : Fragment() {
@@ -29,8 +25,10 @@ class ClubsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_clubs, container, false)
 
         // Set the adapter
@@ -43,7 +41,7 @@ class ClubsFragment : Fragment() {
                 adapter = ClubsAdapter()
             }
         }
-        clubsViewModel.clubsState.observe(viewLifecycleOwner) { it ->
+        clubsViewModel.clubsState.observe(viewLifecycleOwner) {
             val recyclerView = view as RecyclerView
             val adapter = recyclerView.adapter as ClubsAdapter
             adapter.swap(it)
@@ -64,10 +62,10 @@ class ClubsFragment : Fragment() {
         // TODO: Customize parameter initialization
         @JvmStatic
         fun newInstance(columnCount: Int) =
-                ClubsFragment().apply {
-                    arguments = Bundle().apply {
-                        putInt(ARG_COLUMN_COUNT, columnCount)
-                    }
+            ClubsFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
                 }
+            }
     }
 }
